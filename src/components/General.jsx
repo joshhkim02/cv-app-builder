19cb60ce113443230cc848fe297f11e8c376e20c
joshhkim02 <97993478+joshhkim02@users.noreply.github.com>
2025-01-20 16:58:04 -0700

    feat: updated InputField function to be more modular

--- src/components/General.jsx
+++ src/components/General.jsx
@@ -1,21 +1,13 @@
-import PropTypes from 'prop-types';
 import styles from '../styles/general.module.css';
 
-function InputField({ type, id, text, defaultV }) {
+export function InputField({ type, id, text, defaultV, className }) {
   return (
-    <p>
+    <p className={className}>
       <label htmlFor={id}>{text}</label>
       <input type={type} id={id} name={id} placeholder={defaultV} />
     </p>
   );
 }
-
-InputField.propTypes = {
-  type: PropTypes.string.isRequired,
-  id: PropTypes.string.isRequired,
-  text: PropTypes.string.isRequired,
-  defaultV: PropTypes.string,
-};
 
 export default function General() {
   return (
@@ -23,50 +15,58 @@ export default function General() {
       <h1 className={styles.h1}>
         Personal Details
         <div className={styles.div_line}></div>
-        <form>
+        <form className={styles.general_form}>
           <InputField
+            className={styles.input_label}
             type='text'
             id='first_name'
             text='First Name'
             defaultV='John'
           />
           <InputField
+            className={styles.input_label}
             type='text'
             id='last_name'
             text='Last Name'
             defaultV='Doe'
           />
           <InputField
+            className={styles.input_label}
             type='text'
             id='email'
             text='Email'
             defaultV='johndoe@gmail.com'
           />
           <InputField
+            className={styles.input_label}
             type='text'
             id='phone_num'
             text='Phone Number'
             defaultV='123-456-7890'
           />
           <InputField
+            className={styles.input_label}
             type='text'
             id='address'
             text='Address'
             defaultV='230 Same St, Los Angeles CA, 90210'
           />
           <InputField
+            className={styles.input_label}
             type='text'
             id='occupation'
             text='Occupation'
             defaultV='Software Engineer'
           />
           <InputField
+            className={styles.input_label}
             type='text'
             id='linkedin'
             text='LinkedIn'
             defaultV='https://www.linkedin.com/in/username'
           />
           <InputField
+            className={styles.input_label}
             type='text'
             id='portfolio'
             text='Portfolio'
@@ -81,6 +81,7 @@ export default function General() {
           </p>
           <div className={styles.input_file}>
             <InputField
+              className={styles.input_label}
               type='file'
               id='profile_picture'
               text='Profile Picture'
--- src/components/Practical.jsx
+++ src/components/Practical.jsx
@@ -1,9 +1,57 @@
 import styles from '../styles/practical.module.css';
+import { InputField } from './General.jsx';
+
+function JobExperienceForm() {
+  return (
+    <form className={styles.job_experience_form}>
+      <InputField
+        type='text'
+        id='first_name'
+        text='First Name'
+        defaultV='John'
+      />
+      <InputField
+        type='text'
+        id='first_name'
+        text='First Name'
+        defaultV='John'
+      />
+      <InputField
+        type='text'
+        id='first_name'
+        text='First Name'
+        defaultV='John'
+      />
+      <InputField
+        type='text'
+        id='first_name'
+        text='First Name'
+        defaultV='John'
+      />
+      <InputField
+        type='text'
+        id='first_name'
+        text='First Name'
+        defaultV='John'
+      />
+      <InputField
+        type='text'
+        id='first_name'
+        text='First Name'
+        defaultV='John'
+      />
+    </form>
+  );
+}
 
 export default function Practical() {
   return (
     <>
-      <h1 className={styles.h1}>Employment History</h1>
+      <h1 className={styles.h1}>
+        Employment History
+        <div className={styles.div_line}></div>
+        <JobExperienceForm />
+      </h1>
     </>
   );
 }
