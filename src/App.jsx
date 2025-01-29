a7719865bd3a6dd4228cd2719c66b0891f68c76e
joshhkim02 <97993478+joshhkim02@users.noreply.github.com>
2025-01-29 14:01:39 -0800

    feat: ability to download resume in pdf format

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,49 @@ import General from './components/General.jsx';
 import Practical from './components/Practical.jsx';
 import Education from './components/Education.jsx';
 import Navbar from './components/Navbar.jsx';
+import './styles/app.css';
+
+import html2canvas from 'html2canvas';
+import jsPDF from 'jspdf';
+
+function GeneratePDF() {
+  const exportPDF = () => {
+    // Select the element we want to screenshot
+    const input = document.getElementById('resume_page');
+    // Render the element using html2canvas
+    html2canvas(input, {
+      // Optional arguments
+      logging: true,
+      letterRendering: 1,
+      useCORS: true,
+    }).then((canvas) => {
+      const pdfWidth = 175;
+      const pdfHeight = (canvas.height * pdfWidth) / canvas.width;
+      const imgData = canvas.toDataURL('img/png');
+      const pdf = new jsPDF('p', 'mm', 'a4');
+      pdf.addImage(imgData, 'PNG', 0, 0, pdfWidth, pdfHeight);
+      pdf.save('test.pdf');
+    });
+  };
+
+  return (
+    <div className='resume_container'>
+      <div className='pdf_navbar'>
+        <img
+          className='download_resume'
+          src='src/assets/download_icon.png'
+          alt='Download resume button'
+          height='30'
+          width='30'
+          onClick={exportPDF}
+        ></img>
+      </div>
+      <div id='resume_page'>
+        <div className='test'>test</div>
+      </div>
+    </div>
+  );
+}
 
 export default function App() {
   return (
@@ -13,7 +56,7 @@ export default function App() {
         <Education />
       </div>
       <div className='form-container'>
-        <h1>Resume Here</h1>
+        <GeneratePDF />
       </div>
     </div>
   );
